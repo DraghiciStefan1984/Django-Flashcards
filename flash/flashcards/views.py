d6987f10c383e60fe52bbca6f77920ef55ef17f0
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-03-26 08:18:01 +0200

    done

--- flash/flashcards/views.py
+++ flash/flashcards/views.py
@@ -1,17 +1,78 @@
 from django.shortcuts import render
+from random import randint
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
 
 def add(request):
-    return render(request, 'add.html', {})
+    num1=randint(0,10)
+    num2=randint(0,10)
+    if request.method=='POST':
+        answer=request.POST['answer']
+        old_num1=request.POST['old_num1']
+        old_num2=request.POST['old_num2']
+        corrent_answer=int(old_num1)+int(old_num2)
+        if int(answer)==corrent_answer:
+            my_answer='Correct!'
+            color='warning'
+        else:
+            my_answer='Incorrect!'
+            color='danger'
+        return render(request, 'add.html', {'answer':answer, 'my_answer':my_answer, 
+                                            'num1':num1, 'num2':num2, 'color':color})
+    return render(request, 'add.html', {'num1':num1, 'num2':num2})
 
 def subtract(request):
-    return render(request, 'subtract.html', {})
+    num1=randint(0,10)
+    num2=randint(0,10)
+    if request.method=='POST':
+        answer=request.POST['answer']
+        old_num1=request.POST['old_num1']
+        old_num2=request.POST['old_num2']
+        corrent_answer=int(old_num1)-int(old_num2)
+        if int(answer)==corrent_answer:
+            my_answer='Correct!'
+            color='warning'
+        else:
+            my_answer='Incorrect!'
+            color='danger'
+        return render(request, 'subtract.html', {'answer':answer, 'my_answer':my_answer, 
+                                            'num1':num1, 'num2':num2, 'color':color})
+    return render(request, 'subtract.html', {'num1':num1, 'num2':num2})
 
 def multiply(request):
-    return render(request, 'multiply.html', {})
+    num1=randint(0,10)
+    num2=randint(0,10)
+    if request.method=='POST':
+        answer=request.POST['answer']
+        old_num1=request.POST['old_num1']
+        old_num2=request.POST['old_num2']
+        corrent_answer=int(old_num1)*int(old_num2)
+        if int(answer)==corrent_answer:
+            my_answer='Correct!'
+            color='warning'
+        else:
+            my_answer='Incorrect!'
+            color='danger'
+        return render(request, 'multiply.html', {'answer':answer, 'my_answer':my_answer, 
+                                            'num1':num1, 'num2':num2, 'color':color})
+    return render(request, 'multiply.html', {'num1':num1, 'num2':num2})
 
 def divide(request):
-    return render(request, 'divide.html', {})
+    num1=randint(0,10)
+    num2=randint(1,10)
+    if request.method=='POST':
+        answer=request.POST['answer']
+        old_num1=request.POST['old_num1']
+        old_num2=request.POST['old_num2']
+        corrent_answer=int(old_num1)/int(old_num2)
+        if int(answer)==corrent_answer:
+            my_answer='Correct!'
+            color='warning'
+        else:
+            my_answer='Incorrect!'
+            color='danger'
+        return render(request, 'divide.html', {'answer':answer, 'my_answer':my_answer, 
+                                            'num1':num1, 'num2':num2, 'color':color})
+    return render(request, 'divide.html', {'num1':num1, 'num2':num2})
